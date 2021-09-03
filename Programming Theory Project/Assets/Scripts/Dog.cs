f5aea8ec2e872b26a58015e536dae0d3db612713
MichaelRogueH <89492394+MichaelRogueH@users.noreply.github.com>
2021-09-03 11:16:27 -0700

    added Animal Name (Encapsulation)

--- Programming Theory Project/Assets/Scripts/Dog.cs
+++ Programming Theory Project/Assets/Scripts/Dog.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Dog : Animal
 {
+    [SerializeField]
+    private string myName;
+
+   
     // Update is called once per frame
     protected override void Update()
     {
         if (Input.GetKeyDown(KeyCode.K)) // make aninmal speak
         {
-            Debug.Log(" Hi, I am the Dog");
+            Debug.Log(" Hi, I am the Dog. My Name is " + myName);
         }
         base.Update();
     }
