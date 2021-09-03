a5bf7c2c4f69075868b601a1c365be665c69bcef
MichaelRogueH <89492394+MichaelRogueH@users.noreply.github.com>
2021-09-03 11:07:15 -0700

    Implemented Inheritance and abstraction

--- Programming Theory Project/Assets/Scripts/Dog.cs
+++ Programming Theory Project/Assets/Scripts/Dog.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dog : MonoBehaviour
+public class Dog : Animal
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.K)) // make aninmal speak
+        {
+            Debug.Log(" Hi, I am the Dog");
+        }
+        base.Update();
     }
 }
